fac2589c710260ab4572eda4617be7cc444d59f9
yu wang <Yu_Wang@epam.com>
2018-11-27 16:38:57 +0800

    week4-day2-workshop

---
+++ week4/day2/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models.BankOfSimba;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<BankAccount> accountList = new List<BankAccount>()
+        {
+            new BankAccount("Simba", 1000, "lion", "Good",true),
+            new BankAccount("Scar",800,"lion","Bad",false),
+            new BankAccount("Nala",500,"lion","Good",false),
+            new BankAccount("Zazu",100,"bird","Good",false)
+        };
+
+        [HttpGet("/show")]
+        public IActionResult Show()
+        {
+            BankAccount newAccount = new BankAccount("Simba", 2000, "lion", "Good", false);
+            return View(newAccount);
+        }
+
+        [HttpGet("/string")]
+        public IActionResult String()
+        {
+            return View();
+        }
+
+        [HttpGet("multiple")]
+        public IActionResult MultipleAccounts()
+        {
+            return View(accountList);
+        }
+
+        [HttpGet("form")]
+        public IActionResult Form()
+        {
+            return View(accountList);
+        }
+
+        [HttpPost("form")]
+        public IActionResult RaiseAccount(string animal)
+        {
+            foreach (var bankAccount in accountList)
+            {
+                if (bankAccount.Name == animal && bankAccount.King == false)
+                {
+                    string newBalance = (float.Parse(bankAccount.Balance) + 10).ToString("F");
+                    bankAccount.Balance = newBalance;
+                }
+                else if (bankAccount.Name == animal && bankAccount.King == true)
+                {
+                    string newBalance = (float.Parse(bankAccount.Balance) + 100).ToString("F");
+                    bankAccount.Balance = newBalance;
+                }
+            }
+            return RedirectToAction(nameof(MultipleAccounts));
+        }
+
+        [HttpGet("add")]
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddAccount(BankAccount account)
+        {
+            string keep2Decimal = float.Parse(account.Balance).ToString("F");
+            account.Balance = keep2Decimal;
+            accountList.Add(account);
+            return RedirectToAction(nameof(MultipleAccounts));
+        }
+
+    }
+}
---
+++ week4/day2/BankOfSimba/BankOfSimba/Models/BankOfSimba/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models.BankOfSimba
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public string Balance { get; set; }
+        public string AnimalType { get; set; }
+        public string Currency { get; set; }
+        public string PersonalImage { get; set; }
+        public  bool King { get; private set; }
+        public BankAccount(string name, float balance, string type,string image, bool king)
+        {
+            this.Name = name;
+            this.Balance = balance.ToString("F");
+            this.AnimalType = type;
+            this.Currency = "zebra";
+            this.PersonalImage = image;
+            this.King = king;
+        }
+
+        public BankAccount()
+        {
+            this.Currency = "zebra";
+
+        }
+
+    }
+}
